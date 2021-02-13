990d4276197027d0fe97346d4b5b5b34fd243031
ladyhodhod <ladyhodhod@gmail.com>
2021-02-13 21:16:34 +0300

    added parts to the model

--- app/models/proverb.rb
+++ app/models/proverb.rb
@@ -1,2 +1,10 @@
 class Proverb < ApplicationRecord
+
+    # Make sure that both english proverb and translation are included
+    validates_presence_of :english, :translation
+
+    # Scopes that we can use ...
+    scope :alphabetical, ->{order('english')}
+    scope :active, ->{where(active: true)}
+
 end
